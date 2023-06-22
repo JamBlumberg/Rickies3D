de7af66aa6f71c16234ea375a73f502bbe4986c0
JamBerg-Shockoe <jamie.blumberg@shockoe.com>
2023-06-22 09:34:32 -0400

    Initial Commit

---
+++ Rickies 3D/Rickies_3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rickies_3DApp.swift
+//  Rickies 3D
+//
+//  Created by Tiberia on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Rickies_3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
